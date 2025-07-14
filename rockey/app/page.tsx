6d0dbb2672b49a5fc0036e992e550df61e488006
rockeyrai <rockeyrai234@gmail.com>
2025-07-14 19:15:39 +0545

    starting new project with reactjs, tailwind and pnplm

---
+++ rockey/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+<div className="bg-red-400">
+
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <p className="text-lg">Welcome to your Next.js application.</p>
+
+</div>
+  );
+}
